15d15331aa6de931179f328b62315cf0226aa5bf
sanjuanraven15 <163710791+sanjuanraven15@users.noreply.github.com>
2024-03-25 22:01:11 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -10,17 +10,11 @@ const routes = [
   {
     path: '/pending',
     name: 'pending',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/PendingTasks.vue')
   },
   {
     path: '/completed',
     name: 'completed',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/CompletedTasks.vue')
   }
 ]
